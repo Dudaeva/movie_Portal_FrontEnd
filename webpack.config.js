3c8fc664b3095819ace1241c1da18759b879754a
Seda805 <dudaeva44@gmail.com>
2021-08-14 14:30:46 +0300

    сделала расширенную настройку webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: ["@babel/polyfill", "./index.js"],
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [{ loader: "file-loader" }],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 4000,
+  },
+};
